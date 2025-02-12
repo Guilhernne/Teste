b84b3b406054c3b10cf029ae11fb5d29a2319882
Guilherme <marcosfonceka@gmail.com>
2025-02-12 18:45:29 -0300

    Teste

--- java-projeto/src/App.java
+++ java-projeto/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Bom dia pessoal");
+        System.out.println("Bora trabalhar");
     }
 }
